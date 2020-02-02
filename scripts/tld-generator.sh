9ad24c8daf574ec036f89e4ac66b2ba908d1ae4e
Fabian Fröhlich <fafr@uni-bremen.de>
2020-02-02 22:51:22 +0100

    Fixing TLD Replace script Updating TLDs

--- scripts/tld-generator.sh
+++ scripts/tld-generator.sh
@@ -24,13 +24,13 @@
 
 curl https://data.iana.org/TLD/tlds-alpha-by-domain.txt -s | \
     awk {'if ($1 != "#") printf ("%s\n", $1)'} | \
-    sed -e 's/\(.#\)/\L\1/' > .tld.lst
+    sed -e 's/\(.*\)/\L\1/' > .tld.lst
 
 TLD_EXP=$(cat .tld.lst | xargs | sed 's/$/)\$/' | sed 's/^/(/' | sed 's/\s/|/g');
-
 if [ -f "Tests/Fixtures/validTLD.txt" ]; then
-    sed -i -e "s/$TLD_EXP/REPLACE_TLD_BY_INSTALL/" src/Enum/DomainRegExp.php
-    sed -i -e "s/$TLD_EXP/REPLACE_TLD_BY_INSTALL/" Tests/Unit/DomainRegExpTest.php;
+    OLD_TLD_EXP=$(cat Tests/Fixtures/validTLD.txt | xargs | sed 's/$/)\\\$/' | sed 's/^/(/' | sed 's/\s/|/g');
+    sed -i -e "s/$OLD_TLD_EXP/REPLACE_TLD_BY_INSTALL/" src/Enum/DomainRegExp.php
+    sed -i -e "s/$OLD_TLD_EXP/REPLACE_TLD_BY_INSTALL/" Tests/Unit/DomainRegExpTest.php;
 fi
 
 sed -i -e "s/REPLACE_TLD_BY_INSTALL/$TLD_EXP/" src/Enum/DomainRegExp.php
